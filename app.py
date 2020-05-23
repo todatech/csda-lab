6f9c36373eed07f02cdf19ab2c0193c29e654c62
todatech <tonani@gmail.com>
2020-05-23 08:44:56 -0400

    tweak for cloud operations

--- app.py
+++ app.py
@@ -9,7 +9,12 @@ app.config.suppress_callback_exceptions = True
 # Lab 1
 rec = Recommender()
 
-# For Production only
-rec.load_ml_objects()
+# For Production only - save ML model to disk after calcluation
+# faster server reload time
+# rec.load_ml_objects()
+
+# For Production only - DO NOT SAVE ML model to disk, calculate and load into memory
+# save memory when loading
+rec.start_recommender_engine()
 
 df = rec.get_sample_df()
