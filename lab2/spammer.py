aa44adca350e6281789eed787e53df75f6d99ded
todatech <tonani@gmail.com>
2020-06-02 19:49:49 -0400

    completing spamer class

--- lab2/spammer.py
+++ lab2/spammer.py
@@ -43,7 +43,7 @@ class SpamIdentifier:
     def __init__(self):
         self.spam_data = None
         self.algo = None
-        self.tvec = None
+        self.Xctr_tf_fitted = None
         self.start_spam_id_engine()
 
     def save_ml_objects(self):
@@ -53,7 +53,7 @@ class SpamIdentifier:
         logger.info('Saving ML/spam objects to data/ml_spam_obj.pkl')
         
         my_list = [
-            self.tvec,
+            self.Xctr_tf_fitted,
             self.algo,
         ]
         with open(path, 'wb') as output:
@@ -70,7 +70,7 @@ class SpamIdentifier:
                 my_test = pickle.load(input)
             
             # self.genres, 
-            self.tvec, self.algo = my_test
+            self.Xctr_tf_fitted, self.algo = my_test
             logger.info('finish loading ml_objects.pkl.')
         else:
             logger.info('cannot data/ml_spam_obj.pkl, starts engine to populate objects...')
@@ -80,7 +80,7 @@ class SpamIdentifier:
     def start_spam_id_engine(self):
         logger.info('starting spam id engine...')
         self.load_spam_data()
-        self.start_vectorizer()
+        self.build_model()
         logger.info('completing spam id engine...')
     
     def get_sample_ham(self):
@@ -94,24 +94,27 @@ class SpamIdentifier:
         path = os.path.join(mypath, '../data/spam_cleaned.csv')
         self.spam_data = pd.read_csv(path, encoding='latin-1')
 
-    def start_vectorizer(self):
-        logger.info('Loading TF-IDF Vectorizer...')
-        self.tvec = TfidfVectorizer()
-        corpus = self.spam_data['filtered_text'].values.astype('U')
-        X_fitted = self.tvec.fit(corpus)
-        X_transformed = self.tvec.transform(cor)
-        X_word_features = self.tvec.fit_transform()
-        X_word_features_matrix = X_word_features.A
+    def build_model(self):
+        logger.info('Building Train/Test split from spam data...')
+        corpus = self.spam_data.filtered_text.values.astype('U')
         y = self.spam_data.spam.values
-        logger.info('Preparing Train/Test Set...')
-        Xt_train, Xt_test, yt_train, yt_test = train_test_split(X_word_features_matrix, y, test_size=0.25)
+        Xc_train, Xc_test, y_train, y_test = train_test_split(corpus, y, test_size=0.25)
+
+        logger.info('Vectorizing Training Set...')
+        tf = TfidfVectorizer(use_idf=True)
+        self.Xctr_tf_fitted = tf.fit(Xc_train)
+        # should save Xct_tf_fitted in pickle to freeze the vocab list
+        Xctr_tf_fitted_txform = self.Xctr_tf_fitted.transform(Xc_train)
+
+        logger.info('Vectorizing Test Set...')
+        tf_test = TfidfVectorizer(use_idf=True, vocabulary=self.Xctr_tf_fitted.vocabulary_)
+        Xcte_tf_fitted_txform = tf_test.fit_transform(Xc_test)
 
         logger.info('Fitting Linear SVC Model...')
         self.algo = LinearSVC()
-        self.algo.fit(Xt_train, yt_train)
-
-        yt_pred_LSVC = self.algo.predict(Xt_test)
-        LSVC_acc = accuracy_score(yt_test, yt_pred_LSVC)
+        self.algo.fit(Xctr_tf_fitted_txform, y_train)
+        y_pred_LSVC = self.algo.predict(Xcte_tf_fitted_txform)
+        LSVC_acc = accuracy_score(y_test, y_pred_LSVC)
         msg = 'Finalizing Vectorizer, Linear SVC Accuracy: ' + str(LSVC_acc)
         logger.info(msg)
 
@@ -121,6 +124,8 @@ class SpamIdentifier:
         
         filtered_msg = remove_punctuation_and_stopwords(message)
         filtered_msg = ' '.join(filtered_msg)
-
-
-        return filtered_msg
+        tf1 = TfidfVectorizer(use_idf=True, vocabulary=self.Xctr_tf_fitted.vocabulary_)
+        sample_fitted_txform = tf1.fit_transform([filtered_msg])
+        answer = self.algo.predict(sample_fitted_txform)
+        
+        return answer
